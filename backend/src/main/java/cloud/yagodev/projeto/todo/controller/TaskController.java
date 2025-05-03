b7afde0f4a2732bdc3775c3dc19099f3e1328185
Yago Moreira <yagomoreira_dev@yahoo.com>
2025-05-03 20:05:14 -0300

    Adicionado ResponseEntity nos metodos da Controller

--- backend/src/main/java/cloud/yagodev/projeto/todo/controller/TaskController.java
+++ backend/src/main/java/cloud/yagodev/projeto/todo/controller/TaskController.java
@@ -2,8 +2,11 @@ package cloud.yagodev.projeto.todo.controller;
 
 import cloud.yagodev.projeto.todo.dtos.TaskDTO;
 import cloud.yagodev.projeto.todo.services.TaskService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,12 +20,20 @@ public class TaskController {
     }
 
     @GetMapping
-    public List<TaskDTO> buscarTodosController() {
-        return taskService.buscaTodasService();
+    public ResponseEntity<List<TaskDTO>> buscarTodosController() {
+        List<TaskDTO> taskDTOList = taskService.buscaTodasService();
+        return ResponseEntity.ok(taskDTOList);
     }
 
     @PostMapping
-    public TaskDTO inserirController(@RequestBody TaskDTO taskDTO) {
-        return taskService.inserirService(taskDTO);
+    public ResponseEntity<TaskDTO> inserirController(@RequestBody TaskDTO taskDTO) {
+        taskDTO = taskService.inserirService(taskDTO);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(taskDTO.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(taskDTO);
     }
 }
